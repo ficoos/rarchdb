b18fc4db42d28e195786773b2a7795b00ef178a1
twinaphex <libretro@gmail.com>
2014-12-30 09:26:48 +0100

    - Declare variables at the top - Style nits

--- rmsgpack_test.c
+++ rmsgpack_test.c
@@ -7,12 +7,14 @@
 
 #include "rmsgpack.h"
 
-struct stub_state {
+struct stub_state
+{
 	int i;
 	uint64_t stack[256];
 };
 
-static void stub_state_push_map(struct stub_state *s, uint32_t size) {
+static void stub_state_push_map(struct stub_state *s, uint32_t size)
+{
 	s->i++;
 	s->stack[s->i] = 1;
 	s->i++;
@@ -20,7 +22,8 @@ static void stub_state_push_map(struct stub_state *s, uint32_t size) {
 	printf("{");
 }
 
-static void stub_state_push_array(struct stub_state *s, uint32_t size) {
+static void stub_state_push_array(struct stub_state *s, uint32_t size)
+{
 	s->i++;
 	s->stack[s->i] = 2;
 	s->i++;
@@ -29,45 +32,60 @@ static void stub_state_push_array(struct stub_state *s, uint32_t size) {
 }
 
 static void stub_state_pre_print(struct stub_state *s) {}
-static void stub_state_post_print(struct stub_state *s) {
-	switch(s->stack[s->i - 1]) {
-	case 1:
-		if (s->stack[s->i] % 2 == 0) {
-			printf(": ");
-			s->stack[s->i]--;
-		} else if (s->stack[s->i] == 1) {
-			printf("}");
-			s->i -= 2;
-			stub_state_post_print(s);
-		} else {
-			printf(", ");
-			s->stack[s->i]--;
-		}
-		break;
-	case 2:
-		if (s->stack[s->i] == 1) {
-			printf("]");
-			s->i -= 2;
-			stub_state_post_print(s);
-		} else {
-			printf(", ");
-			s->stack[s->i]--;
-		}
-		break;
-	}
+
+static void stub_state_post_print(struct stub_state *s)
+{
+   switch(s->stack[s->i - 1])
+   {
+      case 1:
+         if (s->stack[s->i] % 2 == 0)
+         {
+            printf(": ");
+            s->stack[s->i]--;
+         }
+         else if (s->stack[s->i] == 1)
+         {
+            printf("}");
+            s->i -= 2;
+            stub_state_post_print(s);
+         }
+         else
+         {
+            printf(", ");
+            s->stack[s->i]--;
+         }
+         break;
+      case 2:
+         if (s->stack[s->i] == 1)
+         {
+            printf("]");
+            s->i -= 2;
+            stub_state_post_print(s);
+         }
+         else
+         {
+            printf(", ");
+            s->stack[s->i]--;
+         }
+         break;
+   }
 
 }
 
-static int stub_read_map_start(uint32_t size, void* data) {
+static int stub_read_map_start(uint32_t size, void* data)
+{
 	stub_state_push_map(data, size);
 	return 0;
 }
-static int stub_read_array_start(uint32_t size, void* data) {
+
+static int stub_read_array_start(uint32_t size, void* data)
+{
 	stub_state_push_array(data, size);
 	return 0;
 }
 
-static int stub_read_string(char *s, uint32_t len, void* data) {
+static int stub_read_string(char *s, uint32_t len, void* data)
+{
 	stub_state_pre_print(data);
 	printf("'%s'", s);
 	stub_state_post_print(data);
@@ -75,7 +93,8 @@ static int stub_read_string(char *s, uint32_t len, void* data) {
 	return 0;
 }
 
-static int stub_read_bin(void *s, uint32_t len, void* data) {
+static int stub_read_bin(void *s, uint32_t len, void* data)
+{
 	stub_state_pre_print(data);
 	printf("b'%s'", s);
 	stub_state_post_print(data);
@@ -83,33 +102,36 @@ static int stub_read_bin(void *s, uint32_t len, void* data) {
 	return 0;
 }
 
-static int stub_read_uint(uint64_t value, void* data) {
+static int stub_read_uint(uint64_t value, void* data)
+{
 	stub_state_pre_print(data);
 	printf("%lu", value);
 	stub_state_post_print(data);
 	return 0;
 }
 
-static int stub_read_nil(void* data) {
+static int stub_read_nil(void* data)
+{
 	stub_state_pre_print(data);
 	printf("nil");
 	stub_state_post_print(data);
 	return 0;
 }
-static int stub_read_int(int64_t value, void* data) {
+static int stub_read_int(int64_t value, void* data)
+{
 	stub_state_pre_print(data);
 	printf("%ld", value);
 	stub_state_post_print(data);
 	return 0;
 }
 
-static int stub_read_bool(int value, void* data) {
+static int stub_read_bool(int value, void* data)
+{
 	stub_state_pre_print(data);
-	if (value) {
+	if (value)
 		printf("true");
-	} else {
+	else
 		printf("false");
-	}
 	stub_state_post_print(data);
 	return 0;
 }
